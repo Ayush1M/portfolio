8ccb8301b9e5fec17e16d289f4f1ce72ebb6588b
Ayush <96943871+Ayush1M@users.noreply.github.com>
2024-06-23 23:10:20 +0530

    changes in styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -27,8 +27,8 @@ const Home = () => {
             <div>
             <h2 className="text-6xl mb-4">Hey👋</h2>
             <p className="text-6xl mb-6">My name is <span className="text-orange-500">Ayush</span></p>
-            <p className="text-xl">I build things for the web.</p>
-            <p className="text-xl">Focusing on the accessiblity and responsive design.</p>
+            <p className="text-xl tracking-tight">I build things for the web.</p>
+            <p className="text-xl tracking-tight">Focusing on the accessiblity and responsive design.</p>
             <div className="flex items-center text-6xl mt-20">
                 <LiaLinkedinIn className="p-2 mr-16 rounded-full border-2 border-primary-color bg-primary-color text-secondary-color hover:text-primary-color hover:bg-secondary-color cursor-pointer" />
                 <FaGithub className="p-2 mr-16 rounded-full border-2 border-primary-color bg-primary-color text-secondary-color hover:text-primary-color hover:bg-secondary-color cursor-pointer"/>
@@ -40,7 +40,7 @@ const Home = () => {
         </section>
 
         <section className="mt-40 flex flex-col items-center">
-            <h2 className="mb-16 text-5xl">My Skills</h2>
+            <h2 className="mb-16 text-5xl tracking-tight">My Skills</h2>
             <div className="flex flex-wrap gap-10">
                 <p className="text-xl flex flex-col items-center"><BiLogoHtml5 className="mb-4 text-6xl"/>HTML</p>
                 <p className="text-xl flex flex-col items-center"><BiLogoCss3 className="mb-4 text-6xl"/>CSS</p>
@@ -59,12 +59,12 @@ const Home = () => {
 
 
         <section className="mt-40">
-            <h2 className="text-center text-5xl mb-32">My Projects</h2>
+            <h2 className="text-center text-5xl mb-32 tracking-tight">My Projects</h2>
             <div className="grid grid-cols-3 gap-8 place-items-center">
                 <div>
-                    <img src={project1} alt="todo app" className="w-96 h-96 object-cover rounded-3xl hover:shadow-lg hover:shadow-primary-color" />
+                    <img src={project1} alt="todo app" className="w-80 h-80 object-cover rounded-3xl hover:shadow-lg hover:shadow-primary-color" />   
                     <div className="flex justify-between mt-2">
-                    <h2 className="text-2xl mb-2 tracking-tight">Todo App</h2>
+                    <h2 className="text-2xl mb-2 tracking-tighter">Todo App</h2>
                     <div className="flex mb-4">
                         <p className="text-4xl"><BiLogoReact/></p>
                         <p className="text-4xl"><BiLogoTypescript/></p>
@@ -78,7 +78,7 @@ const Home = () => {
                 </div>
 
                 <div>
-                    <img src={project2} alt="tic tac toe game" className="w-96 h-96 object-cover rounded-3xl hover:shadow-lg hover:shadow-primary-color" />
+                    <img src={project2} alt="tic tac toe game" className="w-80 h-80 object-cover rounded-3xl hover:shadow-lg hover:shadow-primary-color" />
                     <div className="flex justify-between mt-2">
                     <h2 className="text-2xl mb-2 tracking-tighter">Tic Tac Toe Game</h2>
                     <div className="flex mb-4">
@@ -94,9 +94,9 @@ const Home = () => {
                 </div>
 
                 <div>
-                    <img src={project3} alt="quizzical" className="w-96 h-96 object-cover rounded-3xl hover:shadow-lg hover:shadow-primary-color" />
+                    <img src={project3} alt="quizzical" className="w-80 h-80 object-cover rounded-3xl hover:shadow-lg hover:shadow-primary-color" />
                     <div className="flex justify-between mt-2">
-                    <h2 className="text-2xl mb-2 tracking-tight">Quizzical</h2>
+                    <h2 className="text-2xl mb-2 tracking-tighter">Quizzical</h2>
                     <div className="flex mb-4">
                         <p className="text-4xl"><BiLogoReact/></p>
                         <p className="text-4xl"><BiLogoCss3/></p>    
