4e855d2b46ce7951029226dc422ab0578f9e4ec1
Ayush <96943871+Ayush1M@users.noreply.github.com>
2024-06-25 19:05:13 +0530

    added dark/light mode

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
 import Header from "./components/Header"
 import logo from "./assets/logo.png"
 import Home from "./components/Home"
+import { useEffect, useState } from "react"
 
 
 export default function App() {
+
+  const [darkMode, setDarkMode] = useState(false)
+
+  useEffect(() => {
+    if(darkMode){
+      document.documentElement.classList.add('dark')
+    }else {
+      document.documentElement.classList.remove('dark')
+    }
+  }, [darkMode])
+
   return (
-    <div className="font-inconsolata">
-    <Header>
-      <img src={logo} alt="logo image" />
+    <div className="font-inconsolata dark:bg-primary-color">
+    <Header setDarkMode={setDarkMode} darkMode= {darkMode}>
+      <img src={logo} alt="logo image" className="w-24"/>
     </Header>
     <Home />
     </div>
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,14 @@
-const Header = ({children}) => {
+import { MdLightMode } from "react-icons/md";
+import { MdDarkMode } from "react-icons/md";
+
+const Header = ({children, darkMode, setDarkMode}) => {
     return (
-        <header className="w-24 m-6">
+        <header className="p-6 flex justify-between items-center">
             {children}
+            <button className="text-primary-color dark:text-secondary-color text-3xl mr-12" 
+            onClick={() => setDarkMode(!darkMode)}>
+                {darkMode ? <MdLightMode /> : <MdDarkMode />}
+            </button>
         </header>
     )
 }
